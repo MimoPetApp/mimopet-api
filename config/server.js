374bbb13593e5b528c1711b4d5621403d95e63e5
Maurício Lima <mauricio.lima@catho.com>
2021-05-18 22:38:46 -0300

    First Strapicommit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac596ce107e6b2dbd0045e9ff1170055'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b8ca9df9-05bc-4ed2-bf8c-e3334500ef4f'
+};
