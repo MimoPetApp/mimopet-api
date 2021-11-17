050b17d6fc8486f18c9ec839df54297d84587efc
wba25 <wba@cin.ufpe.br>
2021-11-16 22:21:43 -0300

    Add module index by training

--- api/module/controllers/module.js
+++ api/module/controllers/module.js
@@ -5,4 +5,10 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  indexByTraining: async (ctx) => {
+    const { id } = ctx.params;
+    return await strapi.query("module")
+      .find({ trainings: id });
+  }
+};
