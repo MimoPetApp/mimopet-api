67ec4cb52acf44c6ddf566b3960f40a81354b790
wba25 <wba@cin.ufpe.br>
2022-02-05 21:26:18 -0300

    Add status to modules listing

--- api/module/controllers/module.js
+++ api/module/controllers/module.js
@@ -5,10 +5,50 @@
  * to customize this controller
  */
 
+var getModuleStatus = async (module) => {
+  var query = module.steps.map(
+    step => {
+      let payload = {
+        type: step.type
+      }
+      if (step.slides) payload.step = step.slides.id;
+      else if (step.video) payload.step = step.video.id;
+      else if (step.quiz) payload.step = step.quiz.id;
+      else if (step.exercise) payload.step = step.exercise.id;
+      else if (step.feedback) payload.step = step.feedback.id;
+
+      return payload
+    }
+  );
+  
+  query = query.reduce((acc, curr) => {
+    let {type, step} = curr;
+    acc.type = [...(acc.type || [])]
+    acc.step = [...(acc.step || [])]
+    return {type: [...(acc.type || []), type], step: [...(acc.step || []), step]};
+  }, {});
+
+  query.type = Array.from(new Set(query.type));
+  query.step = Array.from(new Set(query.step));
+
+  const stepscompleted = await strapi.query("step-user").find(
+    { ...query, status_eq: "done" }
+  );
+
+  if (stepscompleted.length === 0) return "todo";
+  if (stepscompleted.length === module.steps.length) return "done";
+  
+  return "doing"
+};
+
 module.exports = {
   indexByTraining: async (ctx) => {
     const { id } = ctx.params;
-    return await strapi.query("module")
+    const modules = await strapi.query("module")
       .find({ trainings: id });
+
+    for (let i = 0; i < modules.length; i++)
+      modules[i].status = await getModuleStatus(modules[i]);
+    return modules;
   }
 };
