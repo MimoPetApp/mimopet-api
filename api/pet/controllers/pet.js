b806fe56e7c79ae88888926f07b68551607080ae
wba25 <wba@cin.ufpe.br>
2022-02-06 20:12:44 -0300

    Add pet selection

--- api/pet/controllers/pet.js
+++ api/pet/controllers/pet.js
@@ -4,6 +4,7 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require('strapi-utils');
 
 module.exports = {
   find: async (ctx) => {
@@ -16,6 +17,21 @@ module.exports = {
     await strapi
       .query("user", "users-permissions")
       .update({ id: ctx.state.user.id }, { current_pet: newPet });
-    return newPet;
+    return sanitizeEntity(newPet, {
+      model: strapi.models.pet,
+    });
   },
+  select: async (ctx) => {
+    console.log("oi")
+    const { id } = ctx.params;
+    const pet = await strapi.query("pet")
+      .findOne({ id });
+    
+    await strapi
+     .query("user", "users-permissions")
+     .update({ id: ctx.state.user.id }, { current_pet: pet });
+    return sanitizeEntity(pet, {
+      model: strapi.models.pet,
+    });
+  }
 };
