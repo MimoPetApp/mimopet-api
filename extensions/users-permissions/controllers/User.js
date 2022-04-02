e786942cab28b756630469ed1f520c14ab68c4a5
wba25 <wba@cin.ufpe.br>
2022-04-02 16:00:13 -0300

    Add user verifications

--- extensions/users-permissions/controllers/User.js
+++ extensions/users-permissions/controllers/User.js
@@ -1,4 +1,5 @@
 'use strict';
+const crypto = require('crypto');
 
 /**
  * User.js controller
@@ -7,6 +8,7 @@
  */
 
 const { sanitizeEntity } = require('strapi-utils');
+const { VariableContext } = require("twilio/lib/rest/serverless/v1/service/environment/variable");
 
 const sanitizeUser = user =>
   sanitizeEntity(user, {
@@ -15,47 +17,106 @@ const sanitizeUser = user =>
 
 module.exports = {
   async verifyUser(ctx) {
-    const user = ctx.state.user;
+    var user = ctx.state.user;
     if (!user) {
       return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
     }
-    const resetCode = `${1234}`;
-
+    const confirmationToken = (
+      `${crypto.randomInt(0, 10)}${crypto.randomInt(0, 10)}${crypto.randomInt(0, 10)}${crypto.randomInt(0, 10)}`
+    );
     strapi.config.functions['notification'].sendEmail(
       'noreply@mimopetapp.com',
       user.email,
-      'Welcome 2',
-      `Seu código de reset é : ${resetCode}`
+      'Bem vindo',
+      `Seu código de confirmação de conta é: ${confirmationToken}`
     );
+    user = await strapi
+      .query("user", "users-permissions")
+      .update({ id: user.id }, { confirmationToken: confirmationToken });
     ctx.body = sanitizeUser(user);
   },
   async confirmVerifyUser(ctx) {
-    const user = ctx.state.user;
+    var user = ctx.state.user;
+    const { code } = ctx.request.body;
 
     if (!user) {
       return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
     }
-    // Confirm user
-
+    if (code == user.confirmationToken) {
+      user = await strapi
+      .query("user", "users-permissions")
+      .update(
+        { id: user.id },
+        {
+          confirmed: true,
+          confirmationToken: "",
+          firstAccess: true
+        }
+      );
+    } else {
+      ctx.response.status = 403;
+      return ctx.badRequest(null, [{ messages: [{ id: 'Invalid code' }] }]);
+    }
+    ctx.body = sanitizeUser(user);
+  },
+  async completeFirstAccess(ctx) {
+    var user = ctx.state.user;
+    if (!user) {
+      return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
+    }
+    user = await strapi
+      .query("user", "users-permissions")
+      .update({ id: user.id }, { firstAccess: false });
     ctx.body = sanitizeUser(user);
   },
   async resetPassword(ctx) {
-    const user = ctx.state.user;
+    var user = ctx.state.user;
 
     if (!user) {
       return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
     }
-    // send email with code
+    const resetPasswordToken = (
+      `${crypto.randomInt(0, 10)}${crypto.randomInt(0, 10)}${crypto.randomInt(0, 10)}${crypto.randomInt(0, 10)}`
+    );
+    strapi.config.functions['notification'].sendEmail(
+      'noreply@mimopetapp.com',
+      user.email,
+      'Reset de senha',
+      `Seu código de reset de senha é: ${resetPasswordToken}`
+    );
+    user = await strapi
+      .query("user", "users-permissions")
+      .update(
+        { id: user.id },
+        {
+          verifiedResetPasswordToken: false,
+          resetPasswordToken: resetPasswordToken,
+        }
+      );
 
     ctx.body = sanitizeUser(user);
   },
   async confirmResetPassword(ctx) {
-    const user = ctx.state.user;
+    var user = ctx.state.user;
+    const { code } = ctx.request.body;
 
     if (!user) {
       return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
     }
-    // Confirm user
+    if (code == user.resetPasswordToken) {
+      user = await strapi
+      .query("user", "users-permissions")
+      .update(
+        { id: user.id },
+        {
+          verifiedResetPasswordToken: true,
+          resetPasswordToken: ""
+        }
+      );
+    } else {
+      ctx.response.status = 403;
+      return ctx.badRequest(null, [{ messages: [{ id: 'Invalid code' }] }]);
+    }
 
     ctx.body = sanitizeUser(user);
   },
