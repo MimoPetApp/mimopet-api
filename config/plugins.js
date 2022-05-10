4e9f2c6e3a5f61c94515fdb057242c2c202a14f5
Jadson Lucena <jadsonlucena@gmail.com>
2022-05-10 02:40:59 +0000

    feat (email): sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -2,4 +2,17 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3'
   },
+  email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: process.env.SENDGRID_API_KEY,
+      },
+      settings: {
+        defaultFrom: process.env.DEFAULT_EMAIL,
+        defaultReplyTo: process.env.DEFAULT_EMAIL,
+        testAddress: process.env.DEFAULT_EMAIL
+      },
+    },
+  }
 });
