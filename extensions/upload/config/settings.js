1159a57574ec0d99daa97ab21ec6e159789e004b
Jadson Lucena <jadsonlucena@gmail.com>
2022-06-02 23:15:52 +0000

    build: add settings module google cloud storage

--- extensions/upload/config/settings.js
+++ extensions/upload/config/settings.js
@@ -1,4 +1,4 @@
-module.exports = {
+/*module.exports = {
   provider: 'aws-s3',
   providerOptions: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -8,4 +8,20 @@ module.exports = {
       Bucket: process.env.AWS_UPLOAD_BUCKET,
     },
   },
-};
+};*/
+module.exports = ({ env }) => ({
+    upload: {
+      config: {
+        provider: '@strapi-community/strapi-provider-upload-google-cloud-storage',
+        providerOptions: {
+          serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
+          bucketName: env('GCS_BUCKET_NAME'),
+          basePath: env('GCS_BASE_PATH'),
+          baseUrl: env('GCS_BASE_URL'),
+          publicFiles: env('GCS_PUBLIC_FILES'),
+          uniform: env('GCS_UNIFORM'),
+        },
+      },
+    },
+    //...
+});
