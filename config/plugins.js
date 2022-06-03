20210189042b0760d3b188166f015512f638feae
wellington <wba@cin.ufpe.br>
2022-06-02 21:21:39 -0300

    Update upload provider config

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,36 @@
+// AWS:
+// module.exports = ({ env }) => ({
+//   upload: {
+//     provider: 'aws-s3'
+//   },
+//   email: {
+//     config: {
+//       provider: 'sendgrid',
+//       providerOptions: {
+//         apiKey: process.env.SENDGRID_API_KEY,
+//       },
+//       settings: {
+//         defaultFrom: process.env.MAIL,
+//         defaultReplyTo: process.env.MAIL,
+//         testAddress: process.env.MAIL
+//       },
+//     },
+//   }
+// });
+
+// Google
 module.exports = ({ env }) => ({
   upload: {
-    provider: 'aws-s3'
-  },
-  email: {
     config: {
-      provider: 'sendgrid',
+      provider: "@strapi-community/strapi-provider-upload-google-cloud-storage",
       providerOptions: {
-        apiKey: process.env.SENDGRID_API_KEY,
-      },
-      settings: {
-        defaultFrom: process.env.MAIL,
-        defaultReplyTo: process.env.MAIL,
-        testAddress: process.env.MAIL
+        serviceAccount: env.json("GCS_SERVICE_ACCOUNT"),
+        bucketName: env("GCS_BUCKET_NAME"),
+        basePath: env("GCS_BASE_PATH"),
+        baseUrl: env("GCS_BASE_URL"),
+        publicFiles: env("GCS_PUBLIC_FILES"),
+        uniform: env("GCS_UNIFORM"),
       },
     },
-  }
+  },
 });
